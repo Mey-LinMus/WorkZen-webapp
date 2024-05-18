08917b340bcd46f0ff0b885c2b8b075f000b8fd3
Mey-Lin Mus <mey-lin.mus@student.ehb.be>
2024-05-18 19:55:39 +0200

    DeviceOrientationManager: Implementing Permission Request

--- src/Components/Utils/DeviceOrientationManager.jsx
+++ src/Components/Utils/DeviceOrientationManager.jsx
@@ -4,24 +4,50 @@ const DeviceOrientationManager = ({
   camera,
   renderer,
   scene,
+  onPermissionGranted,
 }) => {
   useEffect(() => {
- 
+    const requestPermission = () => {
+      if (
+        typeof DeviceOrientationEvent !== "undefined" &&
+        typeof DeviceOrientationEvent.requestPermission === "function"
+      ) {
+        DeviceOrientationEvent.requestPermission()
+          .then((response) => {
+            if (response === "granted") {
+              onPermissionGranted(); 
+              window.addEventListener(
+                "deviceorientation",
+                handleDeviceOrientation
+              );
+            }
+          })
+          .catch(console.error);
+      } else {
+        alert("DeviceOrientationEvent is not defined");
+      }
+    };
 
     const handleDeviceOrientation = (event) => {
       const { alpha, beta, gamma } = event;
 
-      camera.rotation.x = (beta * Math.PI) / 180; // Convert degrees to radians
+      camera.rotation.x = (beta * Math.PI) / 180; 
       camera.rotation.y = (gamma * Math.PI) / 180;
       camera.rotation.z = (alpha * Math.PI) / 180;
 
       renderer.render(scene, camera);
     };
 
+    const btn = document.getElementById("request");
+    btn.addEventListener("click", requestPermission);
+
+    requestPermission();
+
     return () => {
+      btn.removeEventListener("click", requestPermission);
       window.removeEventListener("deviceorientation", handleDeviceOrientation);
     };
-  }, [camera, renderer, scene]);
+  }, [camera, renderer, scene, onPermissionGranted]);
 
   return null;
 };
