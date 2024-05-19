93caab0dcaefce5f59ebf583f304fb50e5e6b0de
Mey-Lin Mus <mey-lin.mus@student.ehb.be>
2024-05-19 09:58:10 +0200

    Login: adding  Auth url to login

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import SphereSceneVisual from "./Components/Visuals/SphereSceneVisual";
 import SnowingSceneVisual from "./Components/Visuals/SnowingSceneVisual";
+import MusicSelectPage from "../src/Pages/MusicSelectPage";
 
 function App() {
   return (
     <div className="App">
-      {/* <SphereSceneVisual /> */}
-      <SnowingSceneVisual/>
+      <MusicSelectPage />
     </div>
   );
 }
