273b2c40b4876e307d1d9ec4c5aaeb76056c73ef
Mey-Lin Mus <mey-lin.mus@student.ehb.be>
2024-05-21 23:09:45 +0200

    Music: deleting login button

--- src/Components/Selections/MusicSelect.jsx
+++ src/Components/Selections/MusicSelect.jsx
@@ -1,13 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Dashboard from "../Music/Dashboard";
 import Login from "../Music/Login";
 import "bootstrap/dist/css/bootstrap.min.css";
+
+const URI = "http://localhost:3000";
+const client_id = "1f4f7e164fe945998e2b5904bd676792";
+const AUTH_URL = `https://accounts.spotify.com/authorize?client_id=${client_id}&response_type=code&redirect_uri=${URI}&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state%20user-top-read`;
 
 function MusicSelect() {
   const urlParams = new URLSearchParams(window.location.search);
   const code = urlParams.get("code");
 
-  console.log("Extracted code:", code); 
+  useEffect(() => {
+    if (!code) {
+      window.location.href = AUTH_URL;
+    }
+  }, [code]);
 
   return code ? <Dashboard code={code} /> : <Login />;
 }
