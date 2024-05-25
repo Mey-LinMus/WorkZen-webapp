7b769935a4e612befadfa2f21cbcf1a0d734d9e2
Mey-Lin Mus <mey-lin.mus@student.ehb.be>
2024-05-25 23:44:06 +0200

    VisualSelect: Adding some style

--- src/Components/Selections/VisualSelect.jsx
+++ src/Components/Selections/VisualSelect.jsx
@@ -1,40 +1,43 @@
 import React from "react";
 import { Button } from "react-bootstrap";
-import { useNavigate } from "react-router-dom"; 
+import { useNavigate } from "react-router-dom";
 import visualsData from "../../visuals.json";
 import "../../Styles/visualPage.css";
 
 const VisualSelect = () => {
   const visuals = visualsData.visuals;
-  const navigate = useNavigate(); 
+  const navigate = useNavigate();
 
   const handleVisualClick = (visual) => {
-    localStorage.setItem("selectedVisual", JSON.stringify(visual)); 
-    navigate("/music-select"); 
+    localStorage.setItem("selectedVisual", JSON.stringify(visual));
+    navigate("/music-select");
   };
 
   return (
     <div>
-      {visuals.map((visual) => (
-        <Button
-          key={visual.id}
-          className="video-button"
-          variant="outline-primary"
-          onClick={() => handleVisualClick(visual)}
-        >
-          <video
-            className="video-preview"
-            src={visual.video}
-            type="video/mp4"
-            autoPlay
-            loop
-            muted
+      <h1 className="title-visualSelect">Selecteer een Visual</h1>
+      <div className="button-container">
+        {visuals.map((visual) => (
+          <Button
+            key={visual.id}
+            className="video-button"
+            variant="outline-primary"
+            onClick={() => handleVisualClick(visual)}
           >
-            Your browser does not support the video tag.
-          </video>
-          <p>{visual.title}</p>
-        </Button>
-      ))}
+            <video
+              className="video-preview"
+              src={visual.video}
+              type="video/mp4"
+              autoPlay
+              loop
+              muted
+            >
+              Your browser does not support the video tag.
+            </video>
+            <p>{visual.title}</p>
+          </Button>
+        ))}
+      </div>
     </div>
   );
 };
