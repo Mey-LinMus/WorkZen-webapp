434230ca9651a423e477c137f51c6c47681bc31e
Mey-Lin Mus <mey-lin.mus@student.ehb.be>
2024-05-27 18:17:41 +0200

    Music: changing URI's

--- src/Components/Music/Login.jsx
+++ src/Components/Music/Login.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Container } from "react-bootstrap";
 
-const URI = "http://localhost:3000/music-select";
+const URI = "https://workzen-webapp.onrender.com/music-select";
 const client_id = "1f4f7e164fe945998e2b5904bd676792";
 const AUTH_URL = `https://accounts.spotify.com/authorize?client_id=${client_id}&response_type=code&redirect_uri=${URI}&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state%20user-top-read`;
 
