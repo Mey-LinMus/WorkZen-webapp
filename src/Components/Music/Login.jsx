fc0e9bbd48a296583ebca1a22bb9b3b94bc2ba4f
Mey-Lin Mus <mey-lin.mus@student.ehb.be>
2024-05-27 22:18:12 +0200

    Project: trying to fix deployment

--- src/Components/Music/Login.jsx
+++ src/Components/Music/Login.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-import { Container ,Button} from "react-bootstrap";
+import { Container, Button } from "react-bootstrap";
 
-const URI = "https://workzen-webapp.onrender.com";
+const URI = "https://workzen-webapp.onrender.com/music-select";
 const client_id = "1f4f7e164fe945998e2b5904bd676792";
 const AUTH_URL = `https://accounts.spotify.com/authorize?client_id=${client_id}&response_type=code&redirect_uri=${URI}&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state%20user-top-read`;
 
@@ -10,7 +10,6 @@ export default function Login() {
     <Container
       className="d-flex justify-content-center align-items-center"
       style={{ minHeight: "100vh" }}
-    >
-    </Container>
+    ></Container>
   );
 }
--- src/Components/Music/useAuth.jsx
+++ src/Components/Music/useAuth.jsx
@@ -1,31 +1,31 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
-
 export default function useAuth(code) {
   const [accessToken, setAccessToken] = useState();
   const [refreshToken, setRefreshToken] = useState();
   const [expiresIn, setExpiresIn] = useState();
-  console.log(code);
 
   useEffect(() => {
     axios
-      .post("https://musicserver-iltx.onrender.com/login", { code })
+      .post("http://localhost:8888/login", {
+        code,
+      })
       .then((res) => {
         setAccessToken(res.data.accessToken);
         setRefreshToken(res.data.refreshToken);
         setExpiresIn(res.data.expiresIn);
-        window.history.pushState({}, null, "/music-select");
+        window.history.pushState({}, null, "/");
       })
-      .catch(() => {
-        window.location = "/";
-      });
+      .catch(() => {});
   }, [code]);
 
   useEffect(() => {
     if (!refreshToken || !expiresIn) return;
     const interval = setInterval(() => {
       axios
-        .post("https://musicserver-iltx.onrender.com/refresh", { refreshToken })
+        .post("http://localhost:8888/refresh", {
+          refreshToken,
+        })
         .then((res) => {
           setAccessToken(res.data.accessToken);
           setExpiresIn(res.data.expiresIn);
@@ -34,9 +34,7 @@ export default function useAuth(code) {
           window.location = "/";
         });
     }, (expiresIn - 60) * 1000);
-
     return () => clearInterval(interval);
   }, [refreshToken, expiresIn]);
-
   return accessToken;
 }
