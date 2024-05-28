457610454a1bb133eed1d3efdf2baae3e347ee15
Mey-Lin Mus <mey-lin.mus@student.ehb.be>
2024-05-29 00:23:28 +0200

    VisualSelect: Position h1 and buttons

--- src/Components/Selections/VisualSelect.jsx
+++ src/Components/Selections/VisualSelect.jsx
@@ -2,6 +2,7 @@ import React from "react";
 // import { useNavigate } from "react-router-dom";
 import visualsData from "../../visuals.json";
 import Typography from "../ui-elements/Typography";
+import UILogo from "../ui-elements/Logo";
 
 const VisualSelect = () => {
   const visuals = visualsData.visuals;
@@ -14,16 +15,17 @@ const VisualSelect = () => {
 
   return (
     <div className="bg-primaryColor h-screen flex flex-col items-center">
-      <div className="mt-14 mb-12">
+      <UILogo />
+      <div className="mt-14 mb-12 relative translate-y-10">
         <Typography variant="h2" className="text-center">
           Selecteer een visual
         </Typography>
       </div>
-      <div className="grid grid-cols-2 gap-4 justify-center">
+      <div className="grid grid-cols-2 gap-4 justify-center relative translate-y-10 ">
         {visuals.map((visual) => (
           <button
             key={visual.id}
-            className="relative overflow-hidden rounded-lg"
+            className="relative overflow-hidden rounded-lg transition duration-300 ease-in-out transform hover:scale-105 focus:outline-none"
             style={{ aspectRatio: "1/1", minWidth: "200px" }}
             onClick={() => handleVisualClick(visual)}
           >
