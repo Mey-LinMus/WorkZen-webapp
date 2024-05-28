51bf8a768c0ca6d4f7c64e922bf7709a20502be7
Mey-Lin Mus <mey-lin.mus@student.ehb.be>
2024-05-29 01:34:42 +0200

    StepNavigator: styling

--- src/Components/Selections/StepNavigator.jsx
+++ src/Components/Selections/StepNavigator.jsx
@@ -1,36 +1,54 @@
-import React from "react";
-import { Stepper, Step, Button } from "@material-tailwind/react";
+import { useState } from "react";
+import { HiArrowLeft, HiArrowRight } from "react-icons/hi";
 
+function App() {
+  const [currentStep, setCurrentStep] = useState(1);
 
-function StepNavigator() {
-  const [activeStep, setActiveStep] = React.useState(0);
-  const [isLastStep, setIsLastStep] = React.useState(false);
-  const [isFirstStep, setIsFirstStep] = React.useState(false);
+  const handleNextStep = () => {
+    setCurrentStep(currentStep + 1);
+  };
 
-  const handleNext = () => !isLastStep && setActiveStep((cur) => cur + 1);
-  const handlePrev = () => !isFirstStep && setActiveStep((cur) => cur - 1);
+  const handlePrevStep = () => {
+    setCurrentStep(currentStep - 1);
+  };
 
   return (
-    <div className="w-full py-4 px-8">
-      <Stepper
-        activeStep={activeStep}
-        isLastStep={(value) => setIsLastStep(value)}
-        isFirstStep={(value) => setIsFirstStep(value)}
-      >
-        <Step onClick={() => setActiveStep(0)}>1</Step>
-        <Step onClick={() => setActiveStep(1)}>2</Step>
-        <Step onClick={() => setActiveStep(2)}>3</Step>
-      </Stepper>
-      <div className="mt-16 flex justify-between">
-        <Button onClick={handlePrev} disabled={isFirstStep}>
-          Prev
-        </Button>
-        <Button onClick={handleNext} disabled={isLastStep}>
-          Next
-        </Button>
+    <div className="container mx-auto mt-10 relative translate-y-16">
+      <div className="flex items-center justify-center">
+        <div className="flex items-center">
+          {currentStep > 1 && (
+            <button onClick={handlePrevStep} className="text-gray-500 mr-5">
+              <HiArrowLeft className="w-8 h-8" />
+            </button>
+          )}
+          <div className="flex items-center space-x-5">
+            {[1, 2].map((stepNumber) => (
+              <div
+                key={stepNumber}
+                className={`rounded-full w-10 h-10 flex items-center justify-center ${
+                  stepNumber === currentStep
+                    ? "bg-secondaryColor text-white"
+                    : stepNumber < currentStep
+                    ? "bg-primaryColor text-white"
+                    : "bg-gray-300 text-gray-600"
+                }`}
+              >
+                {stepNumber}
+              </div>
+            ))}
+          </div>
+        </div>
+        {currentStep < 3 && (
+          <button
+            onClick={handleNextStep}
+            className="text-gray-500 ml-5 hover:text-gray-700 transition duration-300"
+          >
+            <HiArrowRight className="w-8 h-8" />
+          </button>
+        )}
       </div>
     </div>
-  )
+  );
 }
 
-export default StepNavigator
+export default App;
