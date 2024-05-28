90af38637b36de9d22986b6e2ed06689f8365a10
Mey-Lin Mus <mey-lin.mus@student.ehb.be>
2024-05-28 18:35:35 +0200

    Welcome: align text

--- src/Components/Home/Welcome.jsx
+++ src/Components/Home/Welcome.jsx
@@ -3,15 +3,19 @@ import Typography from "../ui-elements/Typography";
 
 const Welcome = () => {
   return (
-    <div className="bg-secondaryColor">
-      <div className="">
-        <Typography variant="h2"> Welcome bij WorkZen </Typography>
+    <div className="bg-secondaryColor min-h-[80vh] flex flex-col justify-center items-center text-center">
+      <div className="mb-4">
+        <Typography variant="h2">Welcome bij WorkZen</Typography>
       </div>
-      <div className="">
+      <div className="max-w-xl">
         <Typography variant="bodyText">
           Maak kennis met "WorkZen" – de ultieme webapplicatie die muziek en
           visuals combineert om een unieke ontspanningservaring te creëren.
         </Typography>
+      </div>
+      <div>
+
+        
       </div>
     </div>
   );
