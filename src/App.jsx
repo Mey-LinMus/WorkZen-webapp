8c4516140c97f314e69798ca22822042f5f1c48e
Mey-Lin Mus <mey-lin.mus@student.ehb.be>
2024-05-28 16:46:46 +0200

    Header: positioning items

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import "./App.css";
 import SelectionPage from "./Pages/SelectionPages/SelectionPage";
+import HomePage from "./Pages/HomePage";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-  return <SelectionPage />;
+  return  <HomePage/>;
 }
+  // <SelectionPage />;
+
 
 export default App;
--- src/Components/Home/Header.jsx
+++ src/Components/Home/Header.jsx
@@ -3,14 +3,18 @@ import Logo from "../../Assets/Logo/logo-white.svg";
 
 const Header = () => {
   return (
-    <div className="bg-primaryColor text-neutralColor font-sans flex justify-between items-center px-4 py-2">
-      <div className="flex items-center">
-        <img src={Logo} alt="logo" className="w-12 h-12 mr-2" />
-        <h1 className="font-urbane text-h1 mb-4">WorkZen</h1>
+    <div className="bg-primaryColor text-neutralColor font-sans relative">
+      <div className="flex justify-between items-center px-4 py-2">
+        <div>
+          <img src={Logo} alt="logo" className="w-12 h-12 mr-2" />
+        </div>
+        <button className="bg-neutralColor text-primaryColor font-segoe text-body px-12 rounded-full shadow-lg absolute top-0 right-0 m-4">
+          Start
+        </button>
       </div>
-      <button className="bg-neutralColor text-primaryColor font-segoe text-body">
-        Start
-      </button>
+      <div className="flex justify-center">
+        <h1 className="font-urbane text-h1 mb-0">WorkZen</h1>
+      </div>
     </div>
   );
 };
