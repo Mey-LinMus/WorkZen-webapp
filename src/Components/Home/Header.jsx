d32118563e57b0663bdec32c687ece60f1a3507f
Mey-Lin Mus <mey-lin.mus@student.ehb.be>
2024-05-28 16:50:20 +0200

    Header: fixing position logo

--- src/Components/Home/Header.jsx
+++ src/Components/Home/Header.jsx
@@ -4,14 +4,12 @@ import Logo from "../../Assets/Logo/logo-white.svg";
 const Header = () => {
   return (
     <div className="bg-primaryColor text-neutralColor font-sans relative h-screen flex flex-col justify-center">
-      <div className="flex justify-between items-center px-4 py-2">
-        <div>
-          <img src={Logo} alt="logo" className="w-12 h-12 mr-2" />
-        </div>
-        <button className="bg-neutralColor text-primaryColor font-segoe text-body px-12 rounded-full shadow-lg absolute top-0 right-0 m-4">
-          Start
-        </button>
+      <div className="absolute top-0 left-0 m-4">
+        <img src={Logo} alt="logo" className="w-12 h-12" />
       </div>
+      <button className="bg-neutralColor text-primaryColor font-segoe text-body px-12 rounded-full shadow-lg absolute top-0 right-0 m-4">
+        Start
+      </button>
       <div className="flex justify-center">
         <h1 className="font-urbane text-h1 mb-0">WorkZen</h1>
       </div>
