0fdebaa23cec787014f92beff8726e2e796d8e7a
Mey-Lin Mus <mey-lin.mus@student.ehb.be>
2024-06-03 02:21:05 +0200

    Project: making a PWA

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import React from "react";
-import { createRoot } from "react-dom/client";
-
-import App from "./App";
+import ReactDOM from "react-dom/client";
 import "./index.css";
+import App from "./App";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+import reportWebVitals from "./reportWebVitals";
 
-const root = createRoot(document.getElementById("root"));
-
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+
+
+serviceWorkerRegistration.unregister();
+
+reportWebVitals();
