1cdec925502c6b0d3a86621894fe8932c9e99d5f
Mey-Lin Mus <mey-lin.mus@student.ehb.be>
2024-06-04 10:37:46 +0200

    Auth: adding error handling

--- src/Components/Music/useAuth.jsx
+++ src/Components/Music/useAuth.jsx
@@ -1,5 +1,6 @@
-import { useState, useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
+
 export default function useAuth(code) {
   const [accessToken, setAccessToken] = useState();
   const [refreshToken, setRefreshToken] = useState();
@@ -7,34 +8,35 @@ export default function useAuth(code) {
 
   useEffect(() => {
     axios
-      .post("https://musicserver-iltx.onrender.com/login", {
-        code,
-      })
+      .post("https://musicserver-iltx.onrender.com/login", { code })
       .then((res) => {
         setAccessToken(res.data.accessToken);
         setRefreshToken(res.data.refreshToken);
         setExpiresIn(res.data.expiresIn);
         window.history.pushState({}, null, "/");
       })
-      .catch(() => {});
+      .catch((err) => {
+        console.error("Error during login:", err);
+      });
   }, [code]);
 
   useEffect(() => {
     if (!refreshToken || !expiresIn) return;
     const interval = setInterval(() => {
       axios
-        .post("https://musicserver-iltx.onrender.com/refresh", {
-          refreshToken,
-        })
+        .post("https://musicserver-iltx.onrender.com/refresh", { refreshToken })
         .then((res) => {
           setAccessToken(res.data.accessToken);
           setExpiresIn(res.data.expiresIn);
         })
-        .catch(() => {
+        .catch((err) => {
+          console.error("Error refreshing token:", err);
           window.location = "/";
         });
     }, (expiresIn - 60) * 1000);
+
     return () => clearInterval(interval);
   }, [refreshToken, expiresIn]);
+
   return accessToken;
 }
