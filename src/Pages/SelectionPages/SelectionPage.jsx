26c81fbb8995c4d8ab9b2813e2b2ccb26ece5cf7
Mey-Lin Mus <mey-lin.mus@student.ehb.be>
2024-06-04 10:35:48 +0200

    MakeChoicePage: Setting up page

--- src/Pages/SelectionPages/SelectionPage.jsx
+++ src/Pages/SelectionPages/SelectionPage.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import VisualSelect from "../../Components/Selections/VisualSelect";
 import MusicSelect from "../../Components/Selections/MusicSelect";
+import MakeChoice from "../../Pages/MakeChoicePage";
 import ScenePage from "../ScenePage";
 
 function SelectionPage() {
@@ -10,7 +11,8 @@ function SelectionPage() {
       <Routes>
         <Route path="/scene-page" element={<ScenePage />} />
         <Route path="/music-select" element={<MusicSelect />} />
-        <Route path="/" element={<VisualSelect />} />
+        {/* <Route path="/" element={<VisualSelect />} /> */}
+        <Route path="/" element={<MakeChoice />} />
       </Routes>
     </Router>
   );
