722d25aa7900c9a211906654d7584b85f7718c9f
Mey-Lin Mus <mey-lin.mus@student.ehb.be>
2024-06-05 15:48:42 +0200

    useAuth: adding console.log to see the request amount

--- src/Components/Music/useAuth.jsx
+++ src/Components/Music/useAuth.jsx
@@ -5,16 +5,18 @@ export default function useAuth(code) {
   const [accessToken, setAccessToken] = useState();
   const [refreshToken, setRefreshToken] = useState();
   const [expiresIn, setExpiresIn] = useState();
+  const [requestCount, setRequestCount] = useState(0); // Nieuwe staat voor het bijhouden van het aantal verzoeken
 
   useEffect(() => {
     axios
       .post("https://musicserver-iltx.onrender.com/login", { code })
       .then((res) => {
-        console.log("Login response:", res.data); // Add logging
+        console.log("Login response:", res.data);
         setAccessToken(res.data.accessToken);
         setRefreshToken(res.data.refreshToken);
         setExpiresIn(res.data.expiresIn);
         window.history.pushState({}, null, "/");
+        setRequestCount((prevCount) => prevCount + 1); // Verhoog het aantal verzoeken met 1
       })
       .catch((err) => {
         console.error("Error during login:", err);
@@ -28,9 +30,10 @@ export default function useAuth(code) {
       axios
         .post("https://musicserver-iltx.onrender.com/refresh", { refreshToken })
         .then((res) => {
-          console.log("Refresh response:", res.data); // Add logging
+          console.log("Refresh response:", res.data);
           setAccessToken(res.data.accessToken);
           setExpiresIn(res.data.expiresIn);
+          setRequestCount((prevCount) => prevCount + 1); // Verhoog het aantal verzoeken met 1
         })
         .catch((err) => {
           console.error("Error refreshing token:", err);
@@ -40,6 +43,8 @@ export default function useAuth(code) {
 
     return () => clearInterval(interval);
   }, [refreshToken, expiresIn]);
+
+  console.log("Total requests:", requestCount); // Log het totale aantal verzoeken
 
   return accessToken;
 }
