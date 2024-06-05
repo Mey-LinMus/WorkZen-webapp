6e38c804fe86f62fe9290f208072a859c929aa9f
Mey-Lin Mus <mey-lin.mus@student.ehb.be>
2024-06-05 16:48:27 +0200

    MusicSelect: updating scopes

--- src/Components/Selections/MusicSelect.jsx
+++ src/Components/Selections/MusicSelect.jsx
@@ -4,7 +4,7 @@ import Login from "../Music/Login";
 import Typography from "../ui-elements/Typography";
 const URI = "https://workzen-webapp.onrender.com/music-select";
 const client_id = "1f4f7e164fe945998e2b5904bd676792";
-const AUTH_URL = `https://accounts.spotify.com/authorize?client_id=${client_id}&response_type=code&redirect_uri=${URI}&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state%20user-top-read`;
+const AUTH_URL = `https://accounts.spotify.com/authorize?client_id=${client_id}&response_type=code&redirect_uri=${URI}&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state%20user-top-read%20playlist-read-private%20playlist-read-collaborative`;
 
 function MusicSelect() {
   const urlParams = new URLSearchParams(window.location.search);
