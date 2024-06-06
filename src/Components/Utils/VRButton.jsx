bfa677672cc0fe669d61eaa045448aad9811aab2
Mey-Lin Mus <mey-lin.mus@student.ehb.be>
2024-06-06 15:57:44 +0200

    VRButton: disabling button on desktop

--- src/Components/Utils/VRButton.jsx
+++ src/Components/Utils/VRButton.jsx
@@ -1,15 +1,22 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faVrCardboard } from "@fortawesome/free-solid-svg-icons";
 
 const VRButton = ({ sceneManager }) => {
   const [isStereo, setIsStereo] = useState(false);
+  const [isDesktop, setIsDesktop] = useState(false);
+
+  useEffect(() => {
+    const userAgent = navigator.userAgent;
+    const isDesktopDevice = /Win|Mac|Linux/i.test(userAgent);
+    setIsDesktop(isDesktopDevice);
+  }, []);
 
   const toggleVR = () => {
-    if (!isStereo) {
+    if (!isStereo && !isDesktop) {
       sceneManager.enableStereoEffect();
       setIsStereo(true);
-    } else {
+    } else if (isStereo) {
       sceneManager.disableStereoEffect();
       setIsStereo(false);
     }
@@ -20,9 +27,11 @@ const VRButton = ({ sceneManager }) => {
     <button
       id="vr-toggle"
       onClick={toggleVR}
+      disabled={isDesktop}
+      title={isDesktop ? "VR is not supported on this device" : ""}
       className={`fixed top-2 right-2 z-50 px-4 py-2 text-base font-medium rounded-md cursor-pointer ${
         isStereo ? "bg-green-500 text-white" : "bg-red-500 text-white"
-      }`}
+      } ${isDesktop ? "opacity-50 cursor-not-allowed" : ""}`}
     >
       <FontAwesomeIcon icon={faVrCardboard} />
     </button>
