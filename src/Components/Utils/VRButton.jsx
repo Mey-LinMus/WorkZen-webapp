fd9efa282f1444b1e8674286dc6760f748f6a4a1
Mey-Lin Mus <mey-lin.mus@student.ehb.be>
2024-06-06 16:35:41 +0200

    VRButton: making the button able for mobile devices

--- src/Components/Utils/VRButton.jsx
+++ src/Components/Utils/VRButton.jsx
@@ -4,16 +4,19 @@ import { faVrCardboard } from "@fortawesome/free-solid-svg-icons";
 
 const VRButton = ({ sceneManager }) => {
   const [isStereo, setIsStereo] = useState(false);
-  const [isDesktop, setIsDesktop] = useState(false);
+  const [isMobile, setIsMobile] = useState(false);
 
   useEffect(() => {
     const userAgent = navigator.userAgent;
-    const isDesktopDevice = /Win|Mac|Linux/i.test(userAgent);
-    setIsDesktop(isDesktopDevice);
+    const isMobileDevice =
+      /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+        userAgent
+      );
+    setIsMobile(isMobileDevice);
   }, []);
 
   const toggleVR = () => {
-    if (!isStereo && !isDesktop) {
+    if (!isStereo && isMobile) {
       sceneManager.enableStereoEffect();
       setIsStereo(true);
     } else if (isStereo) {
@@ -27,11 +30,11 @@ const VRButton = ({ sceneManager }) => {
     <button
       id="vr-toggle"
       onClick={toggleVR}
-      disabled={isDesktop}
-      title={isDesktop ? "VR is not supported on this device" : ""}
+      disabled={!isMobile}
+      title={!isMobile ? "VR is not supported on this device" : ""}
       className={`fixed top-2 right-2 z-50 px-4 py-2 text-base font-medium rounded-md cursor-pointer ${
         isStereo ? "bg-green-500 text-white" : "bg-red-500 text-white"
-      } ${isDesktop ? "opacity-50 cursor-not-allowed" : ""}`}
+      } ${!isMobile ? "opacity-50 cursor-not-allowed" : ""}`}
     >
       <FontAwesomeIcon icon={faVrCardboard} />
     </button>
