fe68b09c697127f78d995367ebf2f96eae3979f3
Mey-Lin Mus <mey-lin.mus@student.ehb.be>
2024-06-06 14:06:51 +0200

    NavBar: Adding hamburgermenu

---
+++ src/Components/Navigation/NavigationBar.jsx
@@ -0,0 +1,89 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+
+const NavigationBar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <nav className="bg-primaryColor">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="flex items-center justify-between h-16">
+          <div className="flex items-center">
+            <button
+              onClick={toggleMenu}
+              type="button"
+              className="bg-secondaryColor inline-flex items-center justify-center p-2 rounded-md text-neutralColor hover:text-white hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
+              aria-controls="mobile-menu"
+              aria-expanded="false"
+            >
+              <span className="sr-only">Open main menu</span>
+              {!isOpen ? (
+                <svg
+                  className="block h-6 w-6"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  aria-hidden="true"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M4 6h16M4 12h16M4 18h16"
+                  />
+                </svg>
+              ) : (
+                <svg
+                  className="block h-6 w-6"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  aria-hidden="true"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M6 18L18 6M6 6l12 12"
+                  />
+                </svg>
+              )}
+            </button>
+          </div>
+          <div className="-mr-2 flex"></div>
+        </div>
+      </div>
+
+      <div className={`${isOpen ? "block" : "hidden"}`}>
+        <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
+          <Link
+            to="/favorieten"
+            className="text-neutralColor hover:bg-secondaryColor block px-3 py-2 rounded-md text-base font-medium"
+          >
+            Favorieten
+          </Link>
+          <Link
+            to="/muziek"
+            className="text-neutralColor hover:bg-secondaryColor block px-3 py-2 rounded-md text-base font-medium"
+          >
+            Muziek
+          </Link>
+          <Link
+            to="/visual"
+            className="text-neutralColor hover:bg-secondaryColor block px-3 py-2 rounded-md text-base font-medium"
+          >
+            Visual
+          </Link>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default NavigationBar;
