fa71f3f218ecd966975565e3af77a1b252ff3bbf
Mey-Lin Mus <mey-lin.mus@student.ehb.be>
2024-06-07 15:49:11 +0200

    Favorites Getting favorite in console

--- src/Pages/FavoritesPage.jsx
+++ src/Pages/FavoritesPage.jsx
@@ -1,44 +1,53 @@
 import React, { useState, useEffect } from "react";
 
-const FavoritesPage = () => {
-  const [deviceId, setDeviceId] = useState("");
-  const [favorites, setFavorites] = useState([]);
+const FavoritePage = () => {
+  const [favoriteCombination, setFavoriteCombination] = useState(null);
+  const deviceId = localStorage.getItem("deviceId");
 
   useEffect(() => {
-    const generateDeviceId = () => {
-      const id = window.crypto.randomUUID();
-      setDeviceId(id);
-      return id;
+    const fetchFavoriteCombination = async () => {
+      try {
+        const response = await fetch(
+          `https://backend-favorites.onrender.com/favorites/${deviceId}`
+        );
+        if (response.ok) {
+          const data = await response.json();
+          console.log("Favorite combination:", data);
+          setFavoriteCombination(data);
+        } else {
+          console.error("Failed to fetch favorite combination");
+        }
+      } catch (error) {
+        console.error("Error fetching favorite combination", error);
+      }
     };
 
-    const storedDeviceId = window.localStorage.getItem("deviceId");
-    if (storedDeviceId) {
-      setDeviceId(storedDeviceId);
-    } else {
-      const newDeviceId = generateDeviceId();
-      window.localStorage.setItem("deviceId", newDeviceId);
-      setDeviceId(newDeviceId);
-    }
-  }, []);
-
-  useEffect(() => {
-    if (deviceId) {
-      fetch(`https://backend-favorites.onrender.com/favorites/${deviceId}`)
-        .then((response) => response.json())
-        .then((data) => setFavorites(data.favorites || []));
-    }
+    fetchFavoriteCombination();
   }, [deviceId]);
 
   return (
     <div>
-      <h2>Favorites</h2>
-      <ul>
-        {favorites.map((favorite, index) => (
-          <li key={index}>{JSON.stringify(favorite)}</li>
-        ))}
-      </ul>
+      <h2>Favorieten</h2>
+      {favoriteCombination ? (
+        <div>
+          <h3>Visual:</h3>
+          <p>{favoriteCombination.visual}</p>
+          <h3>Tracks:</h3>
+          <ul>
+            {favoriteCombination.tracks ? (
+              favoriteCombination.tracks.map((track, index) => (
+                <li key={index}>{track}</li>
+              ))
+            ) : (
+              <li>No tracks found</li>
+            )}
+          </ul>
+        </div>
+      ) : (
+        <p>Loading...</p>
+      )}
     </div>
   );
 };
 
-export default FavoritesPage;
+export default FavoritePage;
