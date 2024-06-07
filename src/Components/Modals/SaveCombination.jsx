9ee681c831f8e244c835bfad83df5abb6b79a221
Mey-Lin Mus <mey-lin.mus@student.ehb.be>
2024-06-07 18:44:42 +0200

    Styling the saveCombination popup

--- src/Components/Modals/SaveCombination.jsx
+++ src/Components/Modals/SaveCombination.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import StyledButton from "../ui-elements/Button";
 
 const SaveCombinationModal = ({ show, handleClose, handleSave }) => {
   const [name, setName] = useState("");
@@ -16,11 +17,13 @@ const SaveCombinationModal = ({ show, handleClose, handleSave }) => {
         className="fixed inset-0 bg-black opacity-50"
         onClick={handleClose}
       ></div>
-      <div className="bg-white rounded-lg overflow-hidden shadow-xl transform transition-all max-w-lg w-full p-6">
-        <div className="flex justify-between items-center border-b pb-3">
-          <h3 className="text-lg font-medium">Save Combination</h3>
+      <div className="bg-primaryColor rounded-lg overflow-hidden shadow-xl transform transition-all max-w-lg w-full p-6">
+        <div className="flex justify-between items-center ">
+          <h3 className="text-lg font-medium text-neutralColor">
+            Save combinatie
+          </h3>
           <button
-            className="text-gray-400 hover:text-gray-500"
+            className="text-neutralColor hover:text-gray-500"
             onClick={handleClose}
           >
             <svg
@@ -39,36 +42,31 @@ const SaveCombinationModal = ({ show, handleClose, handleSave }) => {
             </svg>
           </button>
         </div>
-        <div className="mt-4">
-          <label
-            className="block text-sm font-medium text-gray-700 mb-1"
-            htmlFor="combinationName"
-          >
-            Combination Name
-          </label>
+        <div className="mt-6">
           <input
             type="text"
             id="combinationName"
-            placeholder="Enter name"
+            placeholder="Enter combinatie naam"
             value={name}
             onChange={(e) => setName(e.target.value)}
             className="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
           />
-        </div>
-        <div className="mt-4 flex justify-end space-x-2">
-          <button
-            className="bg-gray-300 hover:bg-gray-400 text-gray-700 font-semibold py-2 px-4 rounded"
-            onClick={handleClose}
+
+          <label
+            className="block text-sm font-medium text-slate-300 ml-2 mt-4"
+            htmlFor="combinationName"
           >
-            Cancel
-          </button>
-          <button
+            Geef de combinatie een naam om deze later terug te vinden.
+          </label>
+        </div>
+        <div className="mt-7 flex justify-end space-x-2">
+          <StyledButton
             className="bg-indigo-600 hover:bg-indigo-700 text-white font-semibold py-2 px-4 rounded"
             onClick={onSave}
             disabled={!name}
           >
             Save
-          </button>
+          </StyledButton>
         </div>
       </div>
     </div>
