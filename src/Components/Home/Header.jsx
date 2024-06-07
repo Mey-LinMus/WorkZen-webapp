9f19c042df0fd341220bc2ec17b9fdad527ff5f4
Mey-Lin Mus <mey-lin.mus@student.ehb.be>
2024-06-07 23:45:02 +0200

    Header: changing per 10 seconds videos working

--- src/Components/Home/Header.jsx
+++ src/Components/Home/Header.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import StyledButton from "../ui-elements/Button";
 import Typography from "../ui-elements/Typography";
 import UILogo from "../ui-elements/Logo";
@@ -7,6 +7,23 @@ import { useNavigate } from "react-router-dom";
 const Header = () => {
   const navigate = useNavigate();
 
+  const videoSources = [
+    "/Videos/SnowingScene-horizontal.mp4",
+    "/Videos/SphereScene-horizontal.mp4",
+  ];
+
+  const [currentVideoIndex, setCurrentVideoIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentVideoIndex((prevIndex) =>
+        prevIndex === videoSources.length - 1 ? 0 : prevIndex + 1
+      );
+    }, 10000);
+
+    return () => clearInterval(interval);
+  }, []);
+
   const handleStartClick = () => {
     navigate("/visual-select");
   };
@@ -14,21 +31,22 @@ const Header = () => {
   return (
     <div className="relative h-screen flex flex-col justify-center bg-primaryColor">
       <video
+        key={videoSources[currentVideoIndex]}
         className="absolute top-0 left-0 w-full h-full object-cover z-10 brightness-50"
         autoPlay
         loop
         muted
       >
-        <source src="/Videos/SnowingScene-horizontal.mp4" type="video/mp4" />
+        <source src={videoSources[currentVideoIndex]} type="video/mp4" />
         Your browser does not support the video tag.
       </video>
-      <div className="z-10">
+      <div className="z-20">
         <UILogo />
       </div>
-      <div className="absolute top-0 right-0 m-4 z-10">
+      <div className="absolute top-0 right-0 m-4 z-20">
         <StyledButton onClick={handleStartClick}>Start</StyledButton>
       </div>
-      <div className="flex justify-center px-4 relative z-10">
+      <div className="flex justify-center px-4 relative z-20">
         <Typography variant="h1">WorkZen</Typography>
       </div>
     </div>
