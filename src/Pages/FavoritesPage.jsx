a68392d6b189db2f6f2003d817c1b38bbe945f54
Mey-Lin Mus <mey-lin.mus@student.ehb.be>
2024-06-07 16:36:25 +0200

    Favorites: Getting all the items

--- src/Pages/FavoritesPage.jsx
+++ src/Pages/FavoritesPage.jsx
@@ -1,52 +1,54 @@
 import React, { useState, useEffect } from "react";
 
 const FavoritePage = () => {
-  const [favoriteCombination, setFavoriteCombination] = useState(null);
+  const [favoriteCombinations, setFavoriteCombinations] = useState([]);
   const deviceId = localStorage.getItem("deviceId");
 
   useEffect(() => {
-    const fetchFavoriteCombination = async () => {
+    const fetchFavoriteCombinations = async () => {
       try {
         const response = await fetch(
           `https://backend-favorites.onrender.com/favorites/${deviceId}`
         );
         if (response.ok) {
           const data = await response.json();
-          console.log("Favorite combination:", data);
-          setFavoriteCombination(data);
+          console.log("Favorite combinations:", data);
+          setFavoriteCombinations(data.favorites);
         } else {
-          console.error("Failed to fetch favorite combination");
+          console.error("Failed to fetch favorite combinations");
         }
       } catch (error) {
-        console.error("Error fetching favorite combination", error);
+        console.error("Error fetching favorite combinations", error);
       }
     };
 
-    fetchFavoriteCombination();
+    fetchFavoriteCombinations();
   }, [deviceId]);
 
   return (
     <div>
       <h2>Favorieten</h2>
-      {favoriteCombination ? (
-        <div className="border border-4 border-gray-900">
-          <h3>Visual:</h3>
-          {favoriteCombination.favorites[0].visual && (
-            <div>
-              <p>{favoriteCombination.favorites[0].visual.title}</p>
-            </div>
-          )}
-          <h3>Tracks:</h3>
-          <ul>
-            {favoriteCombination.tracks ? (
-              favoriteCombination.tracks.map((track, index) => (
-                <li key={index}>{track}</li>
-              ))
-            ) : (
-              <li>No tracks found</li>
+      {favoriteCombinations.length > 0 ? (
+        favoriteCombinations.map((favorite, index) => (
+          <div key={index} className="border border-4 border-gray-900">
+            <h3>Visual:</h3>
+            {favorite.visual && (
+              <div>
+                <p>{favorite.visual.title}</p>
+              </div>
             )}
-          </ul>
-        </div>
+            <h3>Tracks:</h3>
+            <ul>
+              {favorite.tracks ? (
+                favorite.tracks.map((track, index) => (
+                  <li key={index}>{track}</li>
+                ))
+              ) : (
+                <li>No tracks found</li>
+              )}
+            </ul>
+          </div>
+        ))
       ) : (
         <p>Loading...</p>
       )}
