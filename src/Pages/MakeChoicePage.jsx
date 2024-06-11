955f7aebf3480e5045bc69a5f497820e0aef7c7f
Mey-Lin Mus <mey-lin.mus@student.ehb.be>
2024-06-11 14:41:49 +0200

    MakeChoicePage: styling the buttons

--- src/Pages/MakeChoicePage.jsx
+++ src/Pages/MakeChoicePage.jsx
@@ -27,31 +27,35 @@ function MakeChoicePage() {
 
   return (
     <div className="bg-gradient-to-b from-custom-gradient-start via-custom-gradient-middle to-custom-gradient-end min-h-screen flex flex-col items-center">
-      <UILogo className="mt-12 md:mt-16 lg:mt-20" />
+      
+      <div className="md:block hidden">
+        <UILogo className="mt-12 md:mt-16 lg:mt-20 " />
+      </div>
+
       <div className="mt-12 md:mt-16 lg:mt-20 text-center">
         <Typography variant="h1" className="text-2xl md:text-3xl lg:text-4xl">
           Maak je keuze
         </Typography>
       </div>
 
-      <div className="mt-8 md:mt-12 lg:mt-16 flex flex-col md:flex-row md:space-x-8 space-y-4 md:space-y-0 items-center">
+      <div className="mt-8 md:mt-12 lg:mt-16 flex flex-row space-x-4 md:space-x-8 items-center">
         <button
           onClick={() => handleOptionClick("custom")}
           selected={selectedOption === "custom"}
-          className="border-solid border-neutralColor border rounded-lg text-neutralColor px-6 py-4 sm:px-8 sm:py-6 lg:px-10 lg:py-8 transition duration-300 ease-in-out transform focus:outline-none px-4 py-2 text-sm md:px-6 md:py-3 md:text-base lg:px-8 lg:py-4 lg:text-lg hover:border-2 active:border-2 focus:ring focus:ring-violet-300"
+          className="border-solid border-neutralColor border rounded-lg text-neutralColor w-24 h-24 sm:w-28 sm:h-28 lg:w-36 lg:h-36 transition duration-300 ease-in-out transform focus:outline-none text-sm md:text-base lg:text-lg hover:border-2 active:border-2 focus:ring focus:ring-violet-300"
         >
           Maak zelf een selectie van liedjes
         </button>
         <button
           onClick={() => handleOptionClick("suggested")}
           selected={selectedOption === "suggested"}
-          className="border-solid border-neutralColor border rounded-lg text-neutralColor px-6 py-4 sm:px-8 sm:py-6 lg:px-10 lg:py-8 transition duration-300 ease-in-out transform focus:outline-none px-4 py-2 text-sm md:px-6 md:py-3 md:text-base lg:px-8 lg:py-4 lg:text-lg hover:border-2 active:border-2 focus:outline-none focus:ring focus:ring-violet-300"
+          className="border-solid border-neutralColor border rounded-lg text-neutralColor w-24 h-24 sm:w-28 sm:h-28 lg:w-36 lg:h-36 transition duration-300 ease-in-out transform focus:outline-none text-sm md:text-base lg:text-lg hover:border-2 active:border-2 focus:ring focus:ring-violet-300"
         >
           Voorgestelde selectie liedjes
         </button>
       </div>
 
-      <div className="mt-28 px-4 py-2 flex space-x-28 items-center ">
+      <div className="mt-28 mb-12 px-4 py-2 flex space-x-28 items-center ">
         <StyledButton onClick={handleBackClick}>
           <HiArrowLeft />
         </StyledButton>
