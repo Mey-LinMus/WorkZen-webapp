1a5795ebbe364279969a2e4de6f013d1acafd49a
Mey-Lin Mus <mey-lin.mus@student.ehb.be>
2024-06-12 00:24:37 +0200

    Homepage: fixing the problem with the Typography

--- src/Components/Home/Welcome.jsx
+++ src/Components/Home/Welcome.jsx
@@ -4,19 +4,18 @@ import Typography from "../ui-elements/Typography";
 const Welcome = () => {
   return (
     <div className="bg-secondaryColor min-h-[80vh] flex flex-col justify-center items-center text-center">
-      <div className="mb-4">
-        <Typography variant="h2">Welcome bij WorkZen</Typography>
-      </div>
-      <div className="max-w-xl">
-        <Typography variant="bodyText">
-          Maak kennis met "WorkZen" – de ultieme webapplicatie die muziek en
-          visuals combineert om een unieke ontspanningservaring te creëren.
+      <div className="mb-4 sm:text-xl">
+        <Typography variant="h2">
+          Welcome bij WorkZen
         </Typography>
       </div>
-      <div>
-
-        
+      <div className="max-w-xl ">
+        <Typography variant="bodyText" className="sm:mx-6 ">
+          Maak kennis met "WorkZen" –<br /> de ultieme webapplicatie die muziek
+          en visuals combineert om een unieke ontspanningservaring te creëren.
+        </Typography>
       </div>
+      <div></div>
     </div>
   );
 };
