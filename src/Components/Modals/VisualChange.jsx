d881db14b21746ceaf8c2e4310b76f9170d8dff3
Mey-Lin Mus <mey-lin.mus@student.ehb.be>
2024-06-11 20:30:30 +0200

    VisualChange: making the popup responsive

--- src/Components/Modals/VisualChange.jsx
+++ src/Components/Modals/VisualChange.jsx
@@ -30,42 +30,45 @@ const VisualChange = ({ onClose, onVisualChange }) => {
 
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-primaryColor bg-opacity-50 z-50">
-      <div className="bg-primaryColor p-8 rounded-lg max-w-3xl w-full text-center ">
-        <Typography variant="h2" className="text-center ">
+      <div className="bg-primaryColor p-4 sm:p-8 rounded-lg max-w-3xl w-full text-center">
+        <Typography variant="h2" className="text-center">
           Change Visual
         </Typography>
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8 justify-center mt-12 ">
-          {visuals.map((visual) => (
-            <button
-              key={visual.id}
-              className={`relative overflow-hidden rounded-lg transition duration-300 ease-in-out transform hover:scale-105 focus:outline-none ${
-                selectedVisual === visual
-                  ? "active:border-8 focus:ring focus:ring-violet-300 scale-110"
-                  : ""
-              }`}
-              style={{
-                aspectRatio: "1/1",
-                minWidth: "200px",
-                maxWidth: "300px",
-                width: "100%",
-              }}
-              onClick={() => handleVisualClick(visual)}
-            >
-              <video
-                className="absolute inset-0 w-full h-full object-cover"
-                src={visual.video}
-                type="video/mp4"
-                autoPlay
-                loop
-                muted
-                playsInline
+        <div className="flex justify-center mt-12 sm:mt-12">
+          <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 gap-4 sm:gap-4">
+            {visuals.map((visual) => (
+              <button
+                key={visual.id}
+                className={`relative overflow-hidden rounded-lg transition duration-300 ease-in-out transform hover:scale-105 focus:outline-none flex items-center ${
+                  selectedVisual === visual
+                    ? "active:border-8 focus:ring focus:ring-violet-300 scale-110"
+                    : ""
+                }`}
+                style={{
+                  aspectRatio: "1/1",
+                  minWidth: "100px",
+                  maxWidth: "100%", // Ensure the buttons are responsive
+                  marginBottom: "0.5rem",
+                }}
+                onClick={() => handleVisualClick(visual)}
               >
-                Your browser does not support the video tag.
-              </video>
-            </button>
-          ))}
+                <video
+                  className="absolute inset-0 w-full h-full object-cover"
+                  src={visual.video}
+                  type="video/mp4"
+                  autoPlay
+                  loop
+                  muted
+                  playsInline
+                >
+                  Your browser does not support the video tag.
+                </video>
+              </button>
+            ))}
+          </div>
         </div>
-        <div className="mt-8 flex justify-end space-x-4">
+
+        <div className="mt-12 sm:mt-8 flex justify-center space-x-4">
           <StyledButton onClick={onClose}>Cancel</StyledButton>
           <StyledButton onClick={handleApplyClick}>Apply</StyledButton>
         </div>
