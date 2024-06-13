abd06bba540b72836173afcd7e3034feef4f4edd
Mey-Lin Mus <mey-lin.mus@student.ehb.be>
2024-06-13 12:22:32 +0200

    VisualSelect: changing the style

--- src/Components/Selections/VisualSelect.jsx
+++ src/Components/Selections/VisualSelect.jsx
@@ -56,7 +56,7 @@ const VisualSelect = () => {
           </Typography>
         </div>
 
-        <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-2 gap-4 justify-items-center w-full mb-12">
+        <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-2 gap-4 justify-items-center w-full ">
           {visuals.map((visual) => (
             <button
               key={visual.id}
@@ -86,7 +86,7 @@ const VisualSelect = () => {
           ))}
         </div>
 
-        <div className="relative mt-16 py-2 flex items-center w-full">
+        <div className="relative mt-16 flex items-center w-full">
           <div className="flex-grow flex justify-center">
             <p className="text-neutralColor mr-12">Stap 1 / 3</p>
           </div>
